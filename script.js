dde14d83dbd5a8bb9aebf3a59d745aef2e392c5a
Arthur Reimus <arthurreimus1623@gmail.com>
2022-04-03 16:17:04 +0800

    Added scoreboard for the game

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
   const choiceArray = ["rock", "paper", "scissors"];
   const randomVal = Math.floor(Math.random() * choiceArray.length);
@@ -6,26 +9,42 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "DRAW";
+    return "ROUND DRAW";
   } else if (
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "rock")
   ) {
-    return "PLAYER ONE WINS!";
+    playerScore++;
+    return "PLAYER ONE WINS THE ROUND!";
   } else if (
     (playerSelection == "rock" && computerSelection == "paper") ||
     (playerSelection == "scissors" && computerSelection == "rock") ||
     (playerSelection == "paper" && computerSelection == "scissors")
   ) {
-    return "YOU LOSE!";
+    computerScore++;
+    return "YOU LOSE THE ROUND!";
   }
 }
 
-for (let i = 0; i < 5; i++) {
-  const computerSelection = computerPlay();
-  const playerSelection = "rock";
-  console.log("Player one: " + playerSelection);
-  console.log("AI: " + computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const computerSelection = computerPlay();
+    const playerSelection = "rock";
+    console.log("Player one: " + playerSelection);
+    console.log("AI: " + computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(
+      "Score\nPlayer: " + playerScore + " Computer: " + computerScore
+    );
+  }
+  if (playerScore > computerScore) {
+    console.log("Congratulations, you won the game!");
+  } else if (playerScore < computerScore) {
+    console.log("You loss. Better luck next time");
+  } else {
+    console.log("DRAW. Try playing again.");
+  }
 }
+
+game(); //play the game 5 times
